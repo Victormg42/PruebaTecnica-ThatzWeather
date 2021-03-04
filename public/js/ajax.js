c099802a45afe672e402fa8c7dc3e59187bbb2b5
Victormg42 <100005779.joan23@fje.edu>
2021-03-04 23:34:09 +0100

    BD sun

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    readTiempo();
-}
-
+        readTiempo();
+    }
+    // Función ajax que usaremos cuando enviemos datos al Controller. //
 function objetoAjax() {
     var xmlhttp = false;
     try {
@@ -18,10 +18,11 @@ function objetoAjax() {
     }
     return xmlhttp;
 }
-
+// Función que leerá el tiempo actual desde la API de OpenWeatherMap y la mostrará por pantalla //
 function readTiempo() {
     var peticion_http = new XMLHttpRequest();
     var section = document.getElementById('tiempo');
+    // Recogemos el valor del input que contiene el codigo postal que hemos enviado previamente por GET //
     var cpostal = document.getElementById('postal').value;
     peticion_http.onreadystatechange = tiempo;
     peticion_http.open('POST', 'http://api.openweathermap.org/data/2.5/weather?zip=' + cpostal + ',es&units=metric&appid=fa47f13f85d5d9731ac73175e669c093&lang=es', true);
@@ -32,17 +33,50 @@ function readTiempo() {
             var tabla = '';
             if (peticion_http.status == 200) {
                 var temp = JSON.parse(peticion_http.responseText);
-                console.log(temp);
-                tabla += '<p>Código postal: <strong>' + cpostal + '</strong></p>';
+                //console.log(temp);
+                // Ejecutamos un innerHTML de todos las lineas de código HTML. //
+                tabla += '<p class="mt-2">Código postal: <strong>' + cpostal + '</strong></p>';
                 tabla += '<p>Ciudad: <strong>' + temp.name + '</strong></p>';
                 tabla += '<input type="hidden" name="nombre" id="nombre" value="' + temp.name + '">';
                 tabla += '<p class="weather_main">' + temp.weather[0].description + '</p>';
                 tabla += "<img class='img_temp' src='http://openweathermap.org/img/w/" + temp.weather[0].icon + ".png'>";
                 tabla += '<p class="weather_temp">' + temp.main.temp + '°</p>';
                 tabla += '<input type="hidden" name="temperatura" id="temperatura" value="' + temp.main.temp + '">';
+                tabla += '<hr>';
+                tabla += '<div class="ml-3 row justify-content-center justify-content-md-start" id="dias">';
+                tabla += '</div>';
             }
             section.innerHTML = tabla;
+            // Llamamos a la función de InsertOrUpdate //
             InsertOrUpdate();
+            readTiempo5dias();
+        }
+    }
+}
+
+function readTiempo5dias() {
+    var peticion_http = new XMLHttpRequest();
+    var section = document.getElementById('dias');
+    var cpostal = document.getElementById('postal').value;
+    peticion_http.onreadystatechange = dias;
+    peticion_http.open('POST', 'http://api.openweathermap.org/data/2.5/forecast?zip=' + cpostal + ',es&units=metric&cnt=5&appid=fa47f13f85d5d9731ac73175e669c093&lang=es', true);
+    peticion_http.send();
+
+    function dias() {
+        if (peticion_http.readyState == 4) {
+            var tabla = '';
+            if (peticion_http.status == 200) {
+                var forecast = JSON.parse(peticion_http.responseText);
+                // console.log(forecast);
+                for (let i = 0; i <= 4; i++) {
+                    tabla += "<img class='img_temp1 float-left' src='http://openweathermap.org/img/w/" + forecast.list[i].weather[0].icon + ".png'>";
+                    tabla += '<div class="texto">';
+                    tabla += '<p>' + forecast.list[i].main.temp + '°</p>';
+                    tabla += '</div>';
+                    tabla += '';
+                }
+            }
+            section.innerHTML = tabla;
         }
     }
 }
@@ -86,12 +120,13 @@ function readTop5() {
             var respuesta = JSON.parse(ajax.responseText);
             var tabla = '';
             console.log(respuesta);
-            tabla += '<h4 class="text-center" style="margin-bottom: 20px;">Top 5 de las zonas más frías según tus busquedas</h4>';
+            tabla += '<h5 class="text-center mt-2" style="margin-bottom: 20px;">Top 5 de las zonas más frías según tus busquedas</h5>';
             for (let i = 0; i < respuesta.length; i++) {
                 tabla += '<p class="top5-temp">' + respuesta[i].temperatura + '°</p>';
-                tabla += '<p>CP: <strong>' + respuesta[i].cp + '</strong></p>';
-                tabla += '<p style="margin-top: -12px;">Ciudad: <strong>' + respuesta[i].nombre + '</strong></p>';
+                tabla += '<p>CP:' + respuesta[i].cp + '</p>';
+                tabla += '<p style="margin-top: -12px;">Ciudad:' + respuesta[i].nombre + '</p>';
                 tabla += '<hr>';
+
             }
             section1.innerHTML = tabla;
         }
--- resources/views/tiempo.blade.php
+++ resources/views/tiempo.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,19 +16,23 @@
     <title>ThatzWeather</title>
 </head>
 <body>
-    <div class="container mt-5">
-    <img class="img-fluid img" src="{{asset('img/Bitmap.png')}}">
+    <div class="container container-sm mt-5">
+        <img class="img-fluid img" src="{{asset('img/Bitmap.png')}}">
+        <form style="margin-top: -6%; margin-left: 10%;" method="GET" action="{{url('/')}}">
+            <input class="btn btn-primary" type="submit" name="Volver" value="Volver">
+        </form>
     </div>
-
+    
     <input type="hidden" name="postal" id="postal" value="{{$_GET['cpostal']}}"></input>
 
     <div class="container tiempo" id="tiempo">
 
     </div>
-
+    
     <div class="container top5" id="top5">
         
     </div>
+    
 
 </body>
 </html>
